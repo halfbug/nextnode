011c5a929ed7de2e4079d92779fb42ac3964a824
Amit Batra <86948970+amit-groupshop@users.noreply.github.com>
2022-12-12 16:17:46 +0530

    Campaign Most Viral Customers (#591)

--- src/groupshops/groupshops.resolver.ts
+++ src/groupshops/groupshops.resolver.ts
@@ -209,6 +209,17 @@ export class GroupshopsResolver {
     }
   }
 
+  @Query(() => [MostViralCustomers], { name: 'mostCampaignViralCustomers' })
+  async mostCampaignViralCustomers(@Args('campaignId') campaignId: string) {
+    if (campaignId !== '') {
+      const data =
+        await this.GroupshopsService.findMostViralCustomersByCampaign(
+          campaignId,
+        );
+      return data;
+    }
+  }
+
   @Query(() => [MostViralProducts], { name: 'mostCampaignViralProducts' })
   async mostCampaignViralProducts(@Args('campaignId') campaignId: string) {
     if (campaignId !== '') {
