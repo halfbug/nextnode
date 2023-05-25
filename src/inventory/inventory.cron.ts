b9adf749623756a72251d2351b248b89714a28ff
Faiza Aziz <92626888+faizaGS@users.noreply.github.com>
2023-05-25 11:10:58 +0500

    GS-2750fix env prod (#885)

--- src/inventory/inventory.cron.ts
+++ src/inventory/inventory.cron.ts
@@ -4,43 +4,47 @@ import { StoresService } from 'src/stores/stores.service';
 import { InventoryService } from './inventory.service';
 import { Injectable, Logger } from '@nestjs/common';
 import { CollectionUpdateEnum } from 'src/stores/entities/store.entity';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SyncCollectionCron {
   constructor(
     private storeService: StoresService,
     private inventryService: InventoryService,
+    private configService: ConfigService,
   ) {}
 
   @Cron(CronExpression.EVERY_HOUR)
   async syncCollections() {
-    log('Cron running');
-    try {
-      const stores = await this.storeService.findWithCollectionUpdate();
-      for (const store of stores) {
-        if (!store.collectionsToUpdate?.length) {
-          Logger.log(
-            `No collections to update in store ${store.shop}`,
-            'SYNC_COLLECTION_CRON',
-            true,
-          );
-          continue;
-        } else {
-          Logger.log(
-            `Sync Collection CRON Started (${store.collectionsToUpdate?.length} needs to update in store ${store.shop})`,
-            'SYNC_COLLECTION_CRON',
-            true,
-          );
+    if (this.configService.get('ENV') === 'production') {
+      log('Cron running');
+      try {
+        const stores = await this.storeService.findWithCollectionUpdate();
+        for (const store of stores) {
+          if (!store.collectionsToUpdate?.length) {
+            Logger.log(
+              `No collections to update in store ${store.shop}`,
+              'SYNC_COLLECTION_CRON',
+              true,
+            );
+            continue;
+          } else {
+            Logger.log(
+              `Sync Collection CRON Started (${store.collectionsToUpdate?.length} needs to update in store ${store.shop})`,
+              'SYNC_COLLECTION_CRON',
+              true,
+            );
 
-          this.storeService.updateStore(store.id, {
-            collectionUpdateStatus: CollectionUpdateEnum.PROGRESS,
-            id: store.id,
-          });
-          await this.inventryService.runSyncCollectionCron(store);
+            this.storeService.updateStore(store.id, {
+              collectionUpdateStatus: CollectionUpdateEnum.PROGRESS,
+              id: store.id,
+            });
+            await this.inventryService.runSyncCollectionCron(store);
+          }
         }
+      } catch (error) {
+        Logger.error(error, 'SYNC_COLLECTION_CRON');
       }
-    } catch (error) {
-      Logger.error(error, 'SYNC_COLLECTION_CRON');
     }
   }
 }
